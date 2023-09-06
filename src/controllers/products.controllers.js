8c7076d4156d63f05a05f21335d920f258a63739
[LucasA] <[lucas.anachuri@gmail.com]>
2023-09-06 19:37:13 -0300

    agrego rutas usuarios

--- src/controllers/products.controllers.js
+++ src/controllers/products.controllers.js
@@ -1,3 +1,30 @@
-export const listarProductos = (req, res) => {
-  res.send("esto es una pruebita");
+import Producto from "../models/product";
+
+export const listarProductos = async (req, res) => {
+  try {
+    // ir a la bd y pedir productos
+    res.send("esto es una pruebita");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const crearProductos = async (req, res) => {
+  try {
+    // ir a la bd y pedir productos
+    console.log(req);
+    console.log(res.body);
+
+    const productoNuevo = new Producto(req.body);
+    //guardar el producto nuevo en la bd
+    await productoNuevo.save();
+    res.status(201).json({
+      mensaje: "el producto fue creado OK",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      mensaje: "el prod no pudo ser creado",
+    });
+  }
 };
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { listarProductos } from "../controllers/productos.controllers.js";
+import {
+  crearProducto,
+  listarProductos,
+} from "../controllers/productos.controllers.js";
 
 const router = Router();
 
-router.route("/producto").get(listarProductos);
-
+router.route("/producto").get(listarProductos).post(crearProducto);
+// router.route('/producto/:id').put()
+// tarea agregar el modelo, ruta y controlador para agregar y listar usuarios
 export default router;
